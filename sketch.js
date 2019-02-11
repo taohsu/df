58080caf7a67e63444a00c9ed7db7b25b567a07d
taohsu <ixmlmxi@gmail.com>
2019-02-11 22:56:12 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,20 +5,37 @@ function preload() {
 }
 
 function setup() {
-  noCanvas(400, 400);
-  createElement('h1', "酥酥每日精选");
+  noCanvas();
+  // createElement('h1', "酥酥每日精选");
+  // var di = createDiv();
+  // di.parent('container');
+  
+  
   var news = data.feeds;
   for (var i = 0; i < news.length; i ++) {
-    createElement('h5', news[i].date);
-  	createElement('h2', news[i].title);
-    createElement('a', news[i].link);
-    createElement('h5', news[i].author);
-    createElement('br');
+  	// createElement('h2', news[i].title);
+    var div0 = createDiv();
+    var title = createA('news[i].link', news[i].title);
+    var author = createElement('h6', '😉' + news[i].author);
+    var date = createElement('h6', news[i].date);
+    var dvd = createElement('hr');
+        // createElement('br');
+    // author.
+      var frame = select('#001');
+  frame.child(div0);
+    title.parent(div0);
+    author.parent(div0);
+    date.parent(div0);
+    dvd.parent(div0);
   }
+  
+
+
+  
   
   // createp;
 }
 
-function draw() {
-  background(220);
-}
+// function draw() {
+//   // background(220);
+// }
